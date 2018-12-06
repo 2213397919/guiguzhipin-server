822229e4251d2b91b79ba74a585de9f6e87edaec
zhangshun <2213397918@qq.com>
2018-12-06 14:41:59 +0800

    添加了user路由

--- router/router.js
+++ router/router.js
@@ -19,13 +19,14 @@ router.post('/register',async (req,res)=>{
             })
         }else{
                 const user = await Users.create({username,password:md5(password),type});
+            //响应一个cookies,保存在本地
                 res.cookie('userid',user.id,{maxAge:1000*3600*24*7});
                 res.json({
                     code:0,
                     data: {
                         username: user.username,
                         _id: user.id,
-                        type: user.type
+                        type: user.type,
                     }
                 })
         }
@@ -45,6 +46,7 @@ router.post('/login',async (req,res)=>{
         //去数据库查找当前用户是否存在
         const user = await Users.findOne({username,password:md5(password)});
         if (user){
+            //响应一个cookies,保存在本地
             res.cookie('userid',user.id,{maxAge:1000*3600*24*7});
             //用户可登录，登录成功
             res.json({
@@ -52,7 +54,12 @@ router.post('/login',async (req,res)=>{
                 data: {
                     username: user.username,
                     _id: user.id,
-                    type: user.type
+                    type: user.type,
+                    header:user.header,
+                    post :user.post,
+                    company :user.company,
+                    salary:user.salary,
+                    info:user.info
                 }
             })
         }else {
@@ -105,4 +112,28 @@ router.post('/update', (req, res) => {
                 res.send({code: 2, msg: '网络不稳定，请重新试试~'})
             })
     })
+// 获取用户信息的路由(根据cookie中的userid)
+router.get('/user', (req, res) => {
+    // 从请求的cookie得到userid
+    const userid = req.cookies.userid
+    // 如果不存在, 直接返回一个提示信息
+    if (!userid) {
+        return res.send({code: 1, msg: '请先登陆'})
+    }
+    // 根据userid查询对应的user
+    Users.findOne({_id: userid}, {__v: 0, password: 0})
+        .then(user => {
+            if (user) {
+                res.send({code: 0, data: user})
+            } else {
+                // 通知浏览器删除userid cookie
+                res.clearCookie('userid')
+                res.send({code: 1, msg: '请先登陆'})
+            }
+        })
+        .catch(error => {
+            console.error('获取用户异常', error)
+            res.send({code: 1, msg: '获取用户异常, 请重新尝试'})
+        })
+})
 module.exports = router;
