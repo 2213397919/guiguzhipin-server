680da3ebdf2b5e76bd23db522e8fe8d10a06980f
zhangshun <2213397918@qq.com>
2018-12-07 10:33:34 +0800

    完成了socket.io搭建

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const express = require('express');
 const router = require('./router/router');
 const db = require('./db');
-
 const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+require('./socket/socket_io')(server)
+server.listen('5000', () => {
+    console.log('socketio服务器启动成功, 请访问: http://localhost:5000')
+});
 
 (async ()=>{
     await db;
@@ -13,7 +19,7 @@ const app = express();
 //监听端口
 app.listen(4000,err=>{
     if (!err){
-        console.log('服务器开启');
+        console.log('服务器开启4000');
     } else {
         console.log(err);
     }
